0c3c5f04f322ad186e05184090dd3f957b9b2b20
willyborja951 <bor_williamalexander@hotmail.com>
2019-10-09 20:35:22 -0500

    cambios

--- routes/login.js
+++ routes/login.js
@@ -5,49 +5,49 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
-app.post('/login',(req,res)=>{
-    let body = req.body
-    console.log(body.email);
-    User.findOne({
-      email:body.email
-    },(err,result)=>{
-      if(err){
-        return res.status(500).json({
-          ok: false,
-          err
-        })
-      }
-      if(result==null){
-        return res.status(400).json({
-          ok:false,
-          result,
-          message:'No se encontro el usuario'
-        })
-      }
-      console.log("body");
-      console.log(body);
-      console.log("res");
-      console.log(result);
+app.post('/login', (req, res) => {
+  let body = req.body
+  console.log(body.email);
+  User.findOne({
+    email: body.email
+  }, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err
+      })
+    }
+    if (result == null) {
+      return res.status(400).json({
+        ok: false,
+        result,
+        message: 'No se encontro el usuario'
+      })
+    }
+    console.log("body");
+    console.log(body);
+    console.log("res");
+    console.log(result);
 
-      const match = bcrypt.compareSync(body.password, result.password) // compara si el registro de labse de datos es igual al que le mandamos en el post
-      // firmamos un token
-      if(match){
-        let token = jwt.sign({
-          // con jwt usando process.env.SEED estamos haciendo la firma
-          user: result
-        }, process.env.SEED,{
+    const match = bcrypt.compareSync(body.password, result.password) // compara si el registro de labse de datos es igual al que le mandamos en el post
+    // firmamos un token
+    if (match) {
+      let token = jwt.sign({
+        // con jwt usando process.env.SEED estamos haciendo la firma
+        user: result
+      }, process.env.SEED, {
           // le damos un tiempo de vidda al token
           expiresIn: process.env.CADUCIDAD
         });
-  
-        res.json({
-          ok: true,
-          usuario: result,
-          token
-        })
-      }
-    })
-  
+
+      return res.json({
+        ok: true,
+        usuario: result,
+        token
+      })
+    }
   })
-  
-  module.exports = app
+
+})
+
+module.exports = app
